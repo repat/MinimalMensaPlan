e22984cd875fa48d47733e5812d0776dd8628c1c
repat <repat@repat.de>
2014-12-09 17:55:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Minimal Mensa Plan
+======
+**Minimal Mensa Plan** is an app for listing the daily lunch in the cantine of the University of Applied Sciences in Hamburg, Campus Berliner Tor. The prices shown are for students
+
+#### Screenshot
+![Screenshot](http://repat.de/Bilder/screenies/minimalmensaplan.png "Screenshot")
+
+#### App Stores
+[![Get it on Google Play](https://raw.github.com/repat/README-template/master/googleplay.png)](https://play.google.com/store/apps/details?id=de.repat.minimalmensaplan)
+
+## Tests
+#### Works on
+* Motorola Moto G
+
+#### Does not work on
+*
+
+## Contributors
+### Artwork
+As the German word for a university cantine is *Mensa* (latin for table) I chose the image [Table Line Art](/detail/76633/table-line-art-by-gammillian) by [gammilian](https://openclipart.org/user-detail/gammillian) from [openclipart.org](https://openclipart.org).
+
+### Third party libraries
+* [jSoup](http://jsoup.org) for parsing the website and extracting the information
+
+## Permissions
+* INTERNET, for scraping the official cantine homepage live
+
+## License 
+* see [LICENSE](https://github.com/username/appname/blob/master/LICENSE.md) file
+
+## Version 
+* Version 1.0
+
+## Contact
+#### Developer/Company
+* Homepage: [http://repat.de](http://repat.de)
+* e-mail: repat@repat.de
+* Twitter: [@repat123](https://twitter.com/twitterhandle "repat123 on twitter")
+
+[![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=repat&url=https://github.com/repat/appname&title=MinimalMensaPlan&language=&tags=github&category=software) 
+
---
+++ src/de/repat/minimalmensaplan/MainActivity.java
@@ -0,0 +1,70 @@
+package de.repat.minimalmensaplan;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+
+	private ArrayList<HashMap<String, String>> dishList = new ArrayList<HashMap<String, String>>();
+	private String date;
+	private TextView dateTextView;
+	private SimpleAdapter simpleAdapter; 
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		dateTextView = (TextView) findViewById(R.id.dateTextView);
+
+		new Network().execute();
+	}
+
+	private class Network extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Document doc = Jsoup
+						.connect(
+								"http://speiseplan.studierendenwerk-hamburg.de/de/520/2014/0/")
+						.get();
+				date = doc.select(".category").text();
+				int maxDishes = doc.getElementsByClass("dish-description")
+						.size();
+				int indexForPrice = 0;
+				for (int i = 0; i < maxDishes; i++) {
+					String dish = doc.getElementsByClass("dish-description")	
+							.get(i).text().replaceAll("(\\W[\\d\\W]*\\W)", " ");
+					String price = doc.getElementsByClass("price")
+							.get(indexForPrice).text();
+					HashMap<String, String> item = new HashMap<String, String>();
+					item.put("dish", dish);
+					item.put("price", price);
+					dishList.add(item);
+					indexForPrice += 2;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			 dateTextView.setText(date);
+			 simpleAdapter = new SimpleAdapter(MainActivity.this,
+			 dishList, R.layout.list, new String[] { "dish",
+			 "price" }, new int[] { R.id.text1, R.id.text2 });
+			
+			 setListAdapter(simpleAdapter);
+		}
+	}
+}
